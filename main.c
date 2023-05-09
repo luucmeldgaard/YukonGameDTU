b6e16fca090696f1400e7d7ee64f51bf1d8e4089
LuucM <luucmeldgaard@outlook.com>
2023-05-09 20:41:31 +0200

    FIXED SDL! IT HAS A PURPOSE! I COULD CRY Yo!

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 #include <SDL.h>
 #include "card.h"
 #include "logic.h"
+#include <SDL_image.h>
+#include <SDL_timer.h>
 #include "printText.h"
 #include "undoRedo.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+SDL_Window* win;
 
 
 
@@ -746,38 +750,163 @@ void startMenu(Card** firstCard, Card** lastCard, char* textBuf){
  * Global values and game initiation.
  */
 
-bool init(SDL_Window* window, SDL_Surface** screenSurface) {
+bool init() {
 
-    // Initialization flag
-    bool success = true;
-
-    //Initialize SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SLD_ERROR: %s\n", SDL_GetError());
-        success = false;
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
     }
-    else {
-        // Create window
-        window = SDL_CreateWindow("Yukon Machine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("SDL window could not be created! SDL_Error: %s\n", SDL_GetError());
-            success = false;
+    win = SDL_CreateWindow("Yukon Game", // creates a window
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       500, 500, 0);
+
+    // triggers the program that controls
+    // your graphics hardware and sets flags
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+
+    // creates a renderer to render our images
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+
+    // creates a surface to load an image into the main memory
+    SDL_Surface* surface;
+    SDL_Surface* surface2;
+
+    surface = IMG_Load("title.png");
+    surface2 = IMG_Load("start.png");
+
+    // loads image to our graphics hardware memory.
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_Texture* tex2 = SDL_CreateTextureFromSurface(rend, surface2);
+
+    // clears main-memory
+    SDL_FreeSurface(surface);
+    SDL_FreeSurface(surface2);
+
+
+    // let us control our image position
+    // so that we can move it with our keyboard.
+    SDL_Rect dest;
+    SDL_Rect dest2;
+
+    // connects our texture with dest to control position
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+    SDL_QueryTexture(tex2, NULL, NULL, &dest2.w, &dest2.h);
+
+    // adjust height and width of title
+    dest.w = 200;
+    dest.h = 200;
+
+    // sets initial x-position of title
+    dest.x = (500 - dest.w) / 2;
+
+    // sets initial y-position of title
+    dest.y = (500 - dest.h) / 2;
+    dest.y -= 200;
+
+
+    // adjust height and width of start
+    dest2.w = 150;
+    dest2.h = 80;
+
+    // sets initial x-position of start
+    dest2.x = (500 - dest2.w) / 2;
+
+    // sets initial y-position of start
+    dest2.y = (500 - dest2.h) / 2;
+
+
+
+    // controls animation loop
+    int close = 0;
+
+    int speed = 5;
+
+    // animation loop
+    while (!close) {
+        SDL_Event event;
+
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+
+                case SDL_QUIT:
+                    // handling of close button
+                    close = 1;
+                    break;
+
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_ENTER)
+                    {
+                        //printf("YOU ARE WITHIN THE WINDOW FRAME");
+                    }
+                    else if (event.window.event == SDL_WINDOWEVENT_LEAVE)
+                    {
+                        //printf("YOU ARE NO LONGER WITHIN THE WINDOW FRAME");
+                    }
+                    break;
+
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT)
+                    {
+                        int x = event.button.x;
+                        int y = event.button.y;
+                        // Handle the left mouse button down event at (x,y)
+                        //printf("x = %d\ny = %d", x, y);
+                        if (x >= dest2.x && x<= dest2.x + dest2.w) {
+                            if (y >= dest2.y && y <= dest2.y + dest2.h) {
+                                close = 1;
+                            }
+                        }
+                    }
+                    break;
+            }
         }
-        else {
-            // Get window surface
-            *screenSurface = SDL_GetWindowSurface(window);
 
-            // Fill the surface with a color
-            SDL_FillRect(*screenSurface, NULL, SDL_MapRGB((*screenSurface)->format, 0xff, 0xff, 0xff));
+        // right boundary
+        if (dest.x + dest.w > 1000)
+            dest.x = 1000 - dest.w;
 
-            // Update the window surface
-            SDL_UpdateWindowSurface(window);
+        // left boundary
+        if (dest.x < 0)
+            dest.x = 0;
 
-            // Get window to stay visible
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-        }
+        // bottom boundary
+        if (dest.y + dest.h > 1000)
+            dest.y = 1000 - dest.h;
 
+        // upper boundary
+        if (dest.y < 0)
+            dest.y = 0;
+
+        // clears the screen
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &dest);
+        SDL_RenderCopy(rend, tex2, NULL, &dest2);
+
+        // triggers the double buffers
+        // for multiple rendering
+        SDL_RenderPresent(rend);
+
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
     }
+
+    // destroy texture
+    SDL_DestroyTexture(tex);
+    SDL_DestroyTexture(tex2);
+
+    // destroy renderer
+    SDL_DestroyRenderer(rend);
+
+    // destroy window
+    SDL_DestroyWindow(win);
+
+    // close SDL
+    SDL_Quit();
+
+    return 0;
+
 }
 
 bool loadMedia(SDL_Window* window, SDL_Surface** screenSurface) {
@@ -806,34 +935,11 @@ bool loadMedia(SDL_Window* window, SDL_Surface** screenSurface) {
     return success;
 }
 
-int main(int argc, char* args[]){
+int main(int argc, char* argv[]){
 
+    // Initialize SDL and start window
+    init();
 
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-
-    // Initialize SDL and get window surface
-    init(window, &screenSurface);
-
-    // Load and apply image to the window surface
-    //loadMedia(window, &screenSurface);
-
-    // Update the window surface
-    SDL_UpdateWindowSurface(window);
-
-    // Keep the window open until the user closes it
-    SDL_Event e;
-    bool quit = false;
-    while (!quit) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-    }
 
 
     /*
@@ -847,14 +953,6 @@ int main(int argc, char* args[]){
     printf("\n\n");
 
     char errorMessages[256];
-
-    // Time test.. should be deleted
-    /*
-    time_t currentTime = time(NULL);
-    srand(currentTime);
-    int tempint = rand() % 52;
-    printf("\n\n%d\n\n", tempint);
-     */
 
 
     /**
